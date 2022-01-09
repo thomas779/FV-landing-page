5e7d42eb5f85cd4ffcc23ca921d1fb2a0f234afc
unknown <thomasehumphreys@gmail.com>
2022-01-10 00:27:22 +0100

    autoplay js script

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ ityped.init(document.querySelector("#ityped"), {
     showCursor: true,
     strings: ['Community Driven.', 'Invest Early.', '100x Opportunities.', 'Exclusive VC Fund.']
   })
+
+var bgvideo = document.getElementById("bgVideo");
+bgvideo.muted = true;
+bgvideo.play();
